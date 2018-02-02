900369aa8e4e20f2287113380049e3f67a3bc0fa
Lance Moreland <lmoreland1899@gmail.com>
2018-02-02 14:24:17 -0500

    Struggling to understand

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,14 @@ public class Main {
 //
 ////        System.out.println(cat.getColor());
 //
-//        Vehicle vehicle = new Vehicle();
+        Vehicle vehicle = new Vehicle();
 //    }
 
-        Pencil pencil = new Pencil(.7, "dixon", true, false, "orange");
-        Pencil pencil1 = new Pencil(.5, "paper mate", true, "purple");
-
-        System.out.println("this is pencil: " + pencil.getColor());
-        System.out.println("this is pencil 1: " + pencil1.getBrand());
+//        Pencil pencil = new Pencil(.7, "dixon", true, false, "orange");
+//        Pencil pencil1 = new Pencil(.5, "paper mate", true, "purple");
+//
+//        System.out.println("this is pencil: " + pencil.getColor());
+//        System.out.println("this is pencil 1: " + pencil1.getBrand());
 
     }
 }
